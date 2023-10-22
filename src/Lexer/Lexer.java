363d889e62617e07305f0075a028b515a68f0633
neyen <elise.neyens@telecomnancy.eu>
2023-10-22 19:29:39 +0200

    Correctifs

--- src/Lexer/Lexer.java
+++ src/Lexer/Lexer.java
@@ -112,6 +112,8 @@ public class Lexer {
                 break;
             case ':':
                 this.newTerminalToken(":", "COLON");
+                this.getNextChar();
+                break;
             case '\n':
                 this.getNextChar();
                 break;
